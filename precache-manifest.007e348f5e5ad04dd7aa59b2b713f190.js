3dabfdb58d9e8690960996488689b490875fb571
Krasen.Ilkov <krasen.ilkov@motion-software.com>
2020-01-09 16:38:59 +0200

    Updates

---
+++ precache-manifest.007e348f5e5ad04dd7aa59b2b713f190.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d4db084ff4e548d095a95532b8d6e9",
+    "url": "/pwa/index.html"
+  },
+  {
+    "revision": "a1bbe07ea20459ad033c",
+    "url": "/pwa/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "546e1b5dc745e8000938",
+    "url": "/pwa/static/js/2.ec517b86.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pwa/static/js/2.ec517b86.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a1bbe07ea20459ad033c",
+    "url": "/pwa/static/js/main.6746773a.chunk.js"
+  },
+  {
+    "revision": "0b5efba28813eae9d7e8",
+    "url": "/pwa/static/js/runtime-main.0866cb42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa/static/media/logo.5d5d9eef.svg"
+  }
+]);
